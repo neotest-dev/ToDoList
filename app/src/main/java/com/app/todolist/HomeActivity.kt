a862e1f4bd5b37d9973ae92e18b57c43771ada4f
neoflorianvecn4 <noeflorian242003@gmail.com>
2024-06-07 17:10:14 -0300

    Autenticación por Correo Firebase R4

---
+++ app/src/main/java/com/app/todolist/HomeActivity.kt
@@ -0,0 +1,37 @@
+package com.app.todolist
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        auth = FirebaseAuth.getInstance()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val logOutButton = findViewById<Button>(R.id.logOutButton)
+        logOutButton.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        val intent = Intent(this, AuthActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
