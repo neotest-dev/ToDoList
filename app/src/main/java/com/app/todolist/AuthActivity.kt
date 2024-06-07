161d41ad57ac99f3e6f4bd146e81c3057e444a31
neoflorianvecn4 <noeflorian242003@gmail.com>
2024-06-07 17:34:30 -0300

    Autenticación por Google Firebase R5

--- app/src/main/java/com/app/todolist/AuthActivity.kt
+++ app/src/main/java/com/app/todolist/AuthActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.app.todolist
 
 import android.content.Intent
@@ -10,17 +12,46 @@ import android.widget.Button
 import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 import android.util.Log
+import android.widget.ImageButton
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseUser
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.GoogleAuthProvider
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.ActivityResultLauncher
 
 class AuthActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 9001
+    private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Base_Theme_ToDoList)
-        auth = FirebaseAuth.getInstance()
         enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
+
+        googleSignInLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleSignInResult(task)
+        }
+        auth = FirebaseAuth.getInstance()
+
+        // Configuración de Google Sign-In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -30,11 +61,13 @@ class AuthActivity : AppCompatActivity() {
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
         val loginButton = findViewById<Button>(R.id.loginButton)
+        val btnSocial = findViewById<ImageButton>(R.id.btnSocial)
+
         signUpButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
             if (email.isNotEmpty() && password.isNotEmpty()) {
-                createAccount(emailEditText.text.toString(), passwordEditText.text.toString())
+                createAccount(email, password)
             } else {
                 Toast.makeText(this, "Rellene los campos", Toast.LENGTH_SHORT).show()
             }
@@ -43,12 +76,58 @@ class AuthActivity : AppCompatActivity() {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
             if (email.isNotEmpty() && password.isNotEmpty()) {
-                signIn(emailEditText.text.toString(), passwordEditText.text.toString())
+                signIn(email, password)
             } else {
                 Toast.makeText(this, "Rellene los campos", Toast.LENGTH_SHORT).show()
             }
         }
+
+        btnSocial.setOnClickListener {
+            signInWithGoogle()
+        }
     }
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        googleSignInLauncher.launch(signInIntent)
+    }
+
+    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)}\n      with the appropriate {@link ActivityResultContract} and handling the result in the\n      {@link ActivityResultCallback#onActivityResult(Object) callback}.")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(Exception::class.java)
+            if (account != null) {
+                firebaseAuthWithGoogle(account.idToken!!)
+            }
+        } catch (e: Exception) {
+            Log.w(TAG, "Google sign in failed", e)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(this, "Autenticación fallida", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
 
     override fun onStart() {
         super.onStart()
@@ -63,20 +142,12 @@ class AuthActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Log.d(TAG, "createUserWithEmail:success")
-                    Toast.makeText(
-                        baseContext,
-                        "Bienvenido",
-                        Toast.LENGTH_SHORT,
-                    ).show()
+                    Toast.makeText(baseContext, "Bienvenido", Toast.LENGTH_SHORT).show()
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Usuario ya existente",
-                        Toast.LENGTH_SHORT,
-                    ).show()
+                    Toast.makeText(baseContext, "Usuario ya existente", Toast.LENGTH_SHORT).show()
                     updateUI(null)
                 }
             }
@@ -87,20 +158,12 @@ class AuthActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Log.d(TAG, "signInWithEmail:success")
-                    Toast.makeText(
-                        baseContext,
-                        "Bienvenido",
-                        Toast.LENGTH_SHORT,
-                    ).show()
+                    Toast.makeText(baseContext, "Bienvenido", Toast.LENGTH_SHORT).show()
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
                     Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Datos incorrectos",
-                        Toast.LENGTH_SHORT,
-                    ).show()
+                    Toast.makeText(baseContext, "Datos incorrectos", Toast.LENGTH_SHORT).show()
                     updateUI(null)
                 }
             }
@@ -111,13 +174,18 @@ class AuthActivity : AppCompatActivity() {
             startActivity(Intent(this, HomeActivity::class.java))
             finish()
         } else {
-            val emailEditText = findViewById<EditText>(R.id.emailEditText)
-            val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
-            emailEditText.text.clear()
-            passwordEditText.text.clear()
-            emailEditText.requestFocus()
+            googleSignInClient.signOut().addOnCompleteListener {
+                googleSignInClient.revokeAccess().addOnCompleteListener {
+                    val emailEditText = findViewById<EditText>(R.id.emailEditText)
+                    val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+                    emailEditText.text.clear()
+                    passwordEditText.text.clear()
+                    emailEditText.requestFocus()
+                }
+            }
         }
     }
+
 
     private fun reload() {
         val user = FirebaseAuth.getInstance().currentUser
--- app/src/main/java/com/app/todolist/HomeActivity.kt
+++ app/src/main/java/com/app/todolist/HomeActivity.kt
@@ -1,21 +1,35 @@
+@file:Suppress("DEPRECATION")
 package com.app.todolist
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 class HomeActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         auth = FirebaseAuth.getInstance()
+
+        // Configuración de Google Sign-In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,9 +43,19 @@ class HomeActivity : AppCompatActivity() {
 
     private fun signOut() {
         auth.signOut()
-        val intent = Intent(this, AuthActivity::class.java)
-        startActivity(intent)
-        finish()
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+            revokeGoogleAccess()
+        }
     }
 
+    private fun revokeGoogleAccess() {
+        googleSignInClient.revokeAccess().addOnCompleteListener(this) {
+            Toast.makeText(this, "Hasta pronto", Toast.LENGTH_SHORT).show()
+            // Redirige a AuthActivity después de revocar el acceso
+            val intent = Intent(this, AuthActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+    }
 }
